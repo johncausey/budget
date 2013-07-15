b75e5f7df5d25889591713815ba3d6c6d3a9da8a
John Causey <martin.causey@gmail.com>
2013-07-15 04:15:43 -0700

    clean up expense chart

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -22,7 +22,7 @@ class Expense < ActiveRecord::Base
   def self.chart_data(start = Time.zone.now.beginning_of_month)
     total_amounts = amounts_by_day(start)
     # month_amounts = where(fixed: true).amounts_by_day(start)
-    (start.to_date..Date.today).map do |date|
+    (start.to_date..Time.zone.now.end_of_month).map do |date|
       {
         date_added: date,
         amount: total_amounts[date] || 0#,
@@ -32,7 +32,7 @@ class Expense < ActiveRecord::Base
   end
 
   def self.amounts_by_day(start)
-    expenses = unscoped.where(date_added: start.beginning_of_day..Time.zone.now)
+    expenses = unscoped.where(date_added: start..Time.zone.now.end_of_month)
     expenses = expenses.group('date(date_added)')
     expenses = expenses.order('date(date_added)')
     expenses = expenses.select('date(date_added) as date_added, sum(amount) as total_amount')
