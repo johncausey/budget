e2f83fb5533ecc1a55370f092c02a7a6c64a2c9f
John Causey <martin.causey@gmail.com>
2013-07-23 04:04:30 -0700

    email sending

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Budget::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'halimede',
+  :password => 'jenny8675309',
+  :domain => 'rainybudget.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Budget::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -27,6 +27,8 @@ Budget::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.default_url_options = { :host => "rainybudget.com" }
   
 end
